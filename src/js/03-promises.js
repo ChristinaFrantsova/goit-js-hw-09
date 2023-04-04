2f5af3945ed408adf063fcfb78f8e9933305a60f
ChristinaFrantsova <mob0964442737@gmail.com>
2023-04-04 19:52:14 +0300

    update 3th task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  // console.log(evt.target.elements);
+  const { delay, step, amount } = evt.target.elements;
+  // console.log(amount.value);
+  let firstDelay = Number(delay.value);
+  let delayStep = Number(step.value);
+
+  for (let i = 1; i <= Number(amount.value); i += 1) {
+    let countDelay = firstDelay + delayStep * (i - 1);
+    createPromise(i, countDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+}
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+        // Fulfill
+      } else {
+        reject({ position, delay });
+        // Reject
+      }
+    }, delay);
+  });
+  return promise;
 }
